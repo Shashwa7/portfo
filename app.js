a8a74875ef9b499bfc235a0616562477eae21cb8
Shashwa7 <55311355+Shashwa7@users.noreply.github.com>
2020-05-28 00:51:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const selectElement = element => document.querySelector(element);
 const hamburger = selectElement('.hamburger');
 const navList = selectElement('.nav-list');
 const navLinks = document.querySelectorAll('.nav-link');
+const body = selectElement('body');
 
 hamburger.addEventListener('click', () => {
 
     hamburger.classList.toggle('toggle');
     navList.classList.toggle('active');
+
+    //freezing scroll
+    body.classList.toggle('screen-freez');
 })
 
 navList.addEventListener('click',()=>{
