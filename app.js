cb9ae140b048aeefbc3343c3b020b0f6da407739
Shashwa7 <55311355+Shashwa7@users.noreply.github.com>
2020-05-28 01:28:37 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('toggle');
     navList.classList.toggle('active');
     /*prventing scree to scroll whil menu is open */
-        body.classList.toggle('screen-freez');
+    body.classList.toggle('screen-freez');
 });
 
 
@@ -19,3 +19,9 @@ navList.addEventListener('click',()=>{
     hamburger.classList.remove('toggle'); /*so that the time symbol goes back to bar symbol */
 });
 
+const removeScrollClass = () => {
+    if(body.classList.contains('screen-freez'))
+    body.classList.remove('screen-freez');
+}
+
+removeScrollClass();
