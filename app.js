4000d9378f3979ee31406be15688165fac2ea4be
Shashwa7 <55311355+Shashwa7@users.noreply.github.com>
2021-06-09 18:31:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('toggle');
     navList.classList.toggle('active');
     /*prventing scree to scroll whil menu is open */
-    body.classList.toggle('screen-freez');
+    body.classList.toggle('screen-freeze');
 });
 
 
@@ -19,10 +19,29 @@ navList.addEventListener('click', () => {
     hamburger.classList.remove('toggle'); /*so that the time symbol goes back to bar symbol */
 
     /*making sure screen-freez class is removed once clicked */
-    body.classList.remove('screen-freez');
+    body.classList.remove('screen-freeze');
 });
 
 //prevent pinch zoom in iphones
  document.addEventListener('gesturestart', (e) => {
             e.preventDefault();
  });
+
+//FIXED / STICKY NAVBAR
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+ // When the user scrolls the page, execute myFunction
+ window.onscroll = function() {myFunction()};
